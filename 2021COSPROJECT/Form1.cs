d132281b6c7e1e4261f3d05faa815440feb3bcc9
marcokruger <marcokruger@W-DT-B3-35>
2021-06-03 09:21:51 +1200

    Add project files.

---
+++ 2021COSPROJECT/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2021COSPROJECT
+{
+    public partial class Form1 : Form
+    {
+        Graphics g; //declare a graphics object called g so we can draw on the Form
+        Spaceship spaceship = new Spaceship(); //create an instance of the Spaceship Class called spaceship
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the Form control
+            g = e.Graphics;
+            //Draw the spaceship
+            spaceship.drawSpaceship(g);
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            spaceship.moveSpaceship(e.X, e.Y);
+        }
+
+        private void tmrSpaceship_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+
+        }
+    }
+}
